192110de07eaac6a7ac45eebf75e21c79e5a6068
Jered Masters <jered.masters@outlook.com>
2018-06-26 08:04:05 +0800

    added alarm fade

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from multiprocessing import Process, Value
 from flask import Flask, render_template
-import datetime
+from datetime import datetime, timedelta
+import time
 import serial
 import time
 import struct
+import platform
 
 app = Flask(__name__)
 status = 0
@@ -31,18 +33,45 @@ def toggle():
     else:
         status.value = 0
 
-def ledLoop(status):
-    ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
-    ser.baudrate=9600
+def writeLed(value):
+    global ser
+    print(value)
+    if ser != 0:
+        ser.write(value.to_bytes(1, byteorder='big'))
+
+def ledLoop(s):
+    global status
+    global ser
+    status = s
+    
+    if platform.system() == "Windows":
+        ser=0
+    else:
+        ser=serial.Serial("/dev/ttyACM0",9600)
+        ser.baudrate=9600
+
+    
 
     prev_status = 100
     while True:
+        time.sleep(0.01)
+        alarm()
         if status.value != prev_status:
-            by = status.value.to_bytes(1, byteorder='big')
-            print(by)
-            ser.write(status.value.to_bytes(1, byteorder='big'))
             prev_status = status.value
-        time.sleep(0.05)
+            writeLed(status.value)            
+
+def alarm():
+    global status
+    now = datetime.now() - timedelta(hours = 2, minutes = 20)
+    h = now.time().hour
+    m = now.time().minute
+    s = now.time().second
+    if h == 5:
+        if m >= 30:
+            s = s + (m - 30) * 60
+            status.value = int((s * 255) / 1860)
+    if h == 6:
+        status.value = 254
 
 if __name__ == '__main__':
     s = Value('i', 0)
